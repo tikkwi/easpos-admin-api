bdb785e89c7d398c85884497d7b5f4c1a18c969c
Tin Ko Ko Win <tinkokowin@ucsy.edu.mm>
2024-09-13 23:33:13 +0630

    update subscription logic

--- src/allowance/allowance.schema.ts
+++ src/allowance/allowance.schema.ts
@@ -2,7 +2,6 @@ import { Schema, SchemaFactory } from '@nestjs/mongoose';
 import { EAllowance } from '@common/utils/enum';
 import { IsBoolean, IsNumber } from 'class-validator';
 import { SchemaTypes } from 'mongoose';
-import { Type } from 'class-transformer';
 import AppProp from '@common/decorator/app_prop.decorator';
 import Allowance from '@shared/allowance/allowance.schema';
 import IsAppEnum from '@common/validator/is_app_enum';
@@ -35,8 +34,7 @@ export default class AppAllowance extends Allowance {
    @AppProp({ type: Boolean, default: false })
    addedUser: boolean;
 
-   @AppProp({ type: SchemaTypes.Mixed })
-   @Type(() => AllowanceBenefit)
+   @AppProp({ type: SchemaTypes.Mixed }, { type: AllowanceBenefit })
    benefit: AllowanceBenefit;
 }
 
--- src/user/admin.schema.ts
+++ src/user/admin.schema.ts
@@ -1,14 +1,12 @@
 import { Schema, SchemaFactory } from '@nestjs/mongoose';
 import { SchemaTypes } from 'mongoose';
-import { Type } from 'class-transformer';
 import { UserRole } from '@common/dto/entity.dto';
 import User from '@shared/user/user.schema';
 import AppProp from '@common/decorator/app_prop.decorator';
 
 @Schema()
 export default class Admin extends User {
-   @AppProp({ type: SchemaTypes.Mixed })
-   @Type(() => UserRole)
+   @AppProp({ type: SchemaTypes.Mixed }, { type: UserRole })
    role: UserRole;
 }
 
