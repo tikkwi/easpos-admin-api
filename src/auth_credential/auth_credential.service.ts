6668d8ce894fd864f89a2450bfa629facd6704f9
tikkwi <tinkokowin@ucsy.edu.mm>
2025-01-14 14:08:37 +0630

    auth credential & basic auth

--- src/auth_credential/auth_credential.service.ts
+++ src/auth_credential/auth_credential.service.ts
@@ -1,9 +1,7 @@
-import { BadRequestException } from '@nestjs/common';
 import {
    AuthCredentialServiceMethods,
    GetAuthCredentialDto,
 } from '@common/dto/auth_credential.dto';
-import { parseGrpcPath } from '@common/utils/regex';
 import AppService from '@common/decorator/app_service.decorator';
 import BaseService from '@common/core/base/base.service';
 import { ModuleRef } from '@nestjs/core';
@@ -22,22 +20,8 @@ export default class AuthCredentialService
       return await repository.create(dto as any);
    }
 
-   async getAuthCredential({ ctx, url, ip }: GetAuthCredentialDto) {
-      const repository = await this.getRepository(ctx.connection, ctx.session);
-      const [_, srv, pth] = parseGrpcPath(url);
-      const isRpc = url.includes('_PACKAGE');
-
-      if (isRpc && !ip) throw new BadRequestException();
-
-      const filter = isRpc
-         ? {
-              allowedPeers: { $elemMatch: { $eq: ip } },
-              authServices: {
-                 $elemMatch: { service: srv, $or: [{ paths: undefined }, { paths: pth }] },
-              },
-           }
-         : { authPaths: { $elemMatch: { $eq: url } } };
-      const { data } = await repository.findOne({ filter });
-      return { data };
+   async getAuthCredential({ ctx: { connection, session }, type }: GetAuthCredentialDto) {
+      const repository = await this.getRepository(connection, session);
+      return await repository.findOne({ filter: { type }, errorOnNotFound: true });
    }
 }
